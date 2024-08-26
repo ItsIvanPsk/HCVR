8696104e7847d577b8e6c98f83544cde349db9d5
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2024-08-26 21:00:56 +0200

    Content Update

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,6 +11,7 @@ public class Checkpoint : MonoBehaviour
     [SerializeField] private CheckpointManager _checkpointManager;
     [SerializeField] private PathMover _mover;
     [SerializeField] private int _checkpointId;
+    [SerializeField] private GameObject _nextCheckpoint;
     [SerializeField] private Content _content;
     [SerializeField] private Guide _guide;
     [SerializeField] private bool _openDoor;
@@ -19,23 +22,30 @@ public class Checkpoint : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        OnCollisionEnterEvent?.Invoke(other);
-        if (_content != null) {
-            _guide.GetComponent<AudioSource>().Stop();
-            _guide.GetComponent<AudioSource>().clip = _content.audio;
-            _guide.GetComponent<AudioSource>().Play();
+        if (gameObject.CompareTag("Checkpoint")) {
+            OnCollisionEnterEvent?.Invoke(other);
+            DisableCheckpoint(other.gameObject);
         }
-        DisableCheckpoint(other.gameObject);
     }
 
     private void DisableCheckpoint(GameObject col)
     {
-        Debug.Log(col.name + " se ha ocultado, checkpoint actual = " + _checkpointId);
         gameObject.GetComponent<BoxCollider>().enabled = false;
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         if (_content.audio != null)
         {
             _guide.LoadAudio(_content.audio);
+            StartCoroutine(DisableNextBlocker(_content.audio.length));
         }
+    }
+
+    public IEnumerator DisableNextBlocker(float duration) {
+        var elapsedTime = 0f;
+        while (elapsedTime < duration) {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        _nextCheckpoint.GetComponent<BoxCollider>().enabled = false;
+        _nextCheckpoint.GetComponent<MeshRenderer>().enabled = false;
     }
 }
--- Assets/Scripts/PathMover.cs
+++ Assets/Scripts/PathMover.cs
@@ -65,7 +65,6 @@ public class PathMover : MonoBehaviour
 
     public void MoveNext()
     {
-        Debug.Log(waitingForNextMove);
         if (waitingForNextMove && currentWaypointIndex < waypoints.Count)
         {
             currentWaypointIndex++;
